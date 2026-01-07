d523d94a5fcf02a8e3292c8d6962208fa103c487
ShlomiAlg <algshlomi@gmail.com>
2026-01-07 22:46:26 +0000

    01 - signal set 1

--- src/main/java/com/forces/ForceTrackingApplication.java
+++ src/main/java/com/forces/ForceTrackingApplication.java
@@ -1,6 +1,7 @@
 package com.forces;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableScheduling
 @SpringBootApplication
